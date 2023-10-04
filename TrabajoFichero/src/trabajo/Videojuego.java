94f964a9914548ae4b1c3e9a3a582050d2990708
j0se-2501 <jolufer23@gmail.com>
2023-10-04 19:09:14 +0200

    clas videojuego

--- TrabajoFichero/src/trabajo/Videojuego.java
+++ TrabajoFichero/src/trabajo/Videojuego.java
@@ -10,16 +10,101 @@ private genero genero;
 private subgenero subgenero; //Es nodo nieto de genero
 private int pegi;
 private int anno;
-private plataforma[] plataforma;
+private String plataforma; //PS5, PS4, PS3, PS2, PS1, PC, xbox, xbox360, xboxOne, xboxOneX, Switch, 
+						//NES, SNES, N64, gameCube, wii, wiiU, megaDrive, saturn, dreamcast;
+						//El String debera quedar, por ejemplo: "PS5, xboxOneX, Switch, PC"
 private int[] jugadores = new int[12]; //un mes por casilla del array
+
+//constructores
+
+public Videojuego(String titulo, String desarrollador, String director, String productor, trabajo.genero genero,
+		trabajo.subgenero subgenero, int pegi, int anno, String plataforma, int[] jugadores) {
+	super();
+	this.titulo = titulo;
+	this.desarrollador = desarrollador;
+	this.director = director;
+	this.productor = productor;
+	this.genero = genero;
+	this.subgenero = subgenero;
+	this.pegi = pegi;
+	this.anno = anno;
+	this.plataforma = plataforma;
+	this.jugadores = jugadores;
+}
+
+//getters y setters
+
+public String getTitulo() {
+	return titulo;
+}
+public void setTitulo(String titulo) {
+	this.titulo = titulo;
+}
+public String getDesarrollador() {
+	return desarrollador;
+}
+public void setDesarrollador(String desarrollador) {
+	this.desarrollador = desarrollador;
+}
+public String getDirector() {
+	return director;
+}
+public void setDirector(String director) {
+	this.director = director;
+}
+public String getProductor() {
+	return productor;
+}
+public void setProductor(String productor) {
+	this.productor = productor;
+}
+public genero getGenero() {
+	return genero;
+}
+public void setGenero(genero genero) {
+	this.genero = genero;
+}
+public subgenero getSubgenero() {
+	return subgenero;
+}
+public void setSubgenero(subgenero subgenero) {
+	this.subgenero = subgenero;
+}
+public int getPegi() {
+	return pegi;
+}
+public void setPegi(int pegi) {
+	this.pegi = pegi;
+}
+public int getAnno() {
+	return anno;
+}
+public void setAnno(int anno) {
+	this.anno = anno;
+}
+public String getPlataforma() {
+	return plataforma;
+}
+public void setPlataforma(String plataforma) {
+	this.plataforma = plataforma;
+}
+public int[] getJugadores() {
+	return jugadores;
+}
+public void setJugadores(int[] jugadores) {
+	this.jugadores = jugadores;
+}
 
 }
 
 
 
+
+//enums
+
 enum plataforma{
 
-	play5, play4, play3, play2, play1, pc, xbox, xbox360, xboxOne, xboxX, xboxS, NintendoSwitch
+	PS5, PS4, PS3, PS2, PS1, PC, xbox, xbox360, xboxOne, xboxOneX, Switch, NES, SNES, N64, gameCube, wii, wiiU, megaDrive, saturn, dreamcast;
 
 }
 
@@ -27,7 +112,7 @@ enum plataforma{
 
 enum genero{
 
-	accion, accionAventura, aventura, puzzle, rol, simulacion, estrategia, deportes
+	accion, accionAventura, aventura, puzzle, rol, simulacion, estrategia, deportes;
 
 }
 
@@ -39,6 +124,12 @@ enum subgenero{
 	actionRPG, JRPG, rolOccidental, MMORPG, roguelike, tacticalRPG, dungeonCrawler, //para rol
 	construccion, vida, vehiculos, //para simulacion
 	guerra, MOBA, realTimeStrategy, turnBasedStrategy, //para estrategia
-	carreras, tenis, futbol, baloncesto, golf //para deportes
+	carreras, tenis, futbol, baloncesto, golf; //para deportes
 
 }
+
+
+
+
+
+
