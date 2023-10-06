93e24500861131a2628857b06bf585d06429ee16
j0se-2501 <jolufer23@gmail.com>
2023-10-06 17:56:06 +0200

    escribir xml ahora coge un nodo en concreto por el titulo

--- TrabajoFichero/src/trabajo/EscribirXML.java
+++ TrabajoFichero/src/trabajo/EscribirXML.java
@@ -2,7 +2,9 @@ package trabajo;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -11,93 +13,114 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import java.io.File; 
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException; 
 
 public class EscribirXML {
+	
+	int nodo;
+    boolean flag;
+    String titulo;
 	
     public void escribirXML(Videojuego videojuegoAEscribir) {
     	
         try {
             // Paso 1: Obtén el documento XML existente
+            
+            nodo=0;
+            flag=false;
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(new File("coleccionvideojuegos.xml"));
+            Document doc;
+    		
+    			doc = builder.parse(new File("coleccionvideojuegos.xml"));
+    		
 
             // Paso 2: Obtén el elemento raíz
             Element rootElement = doc.getDocumentElement();
+            
+            while(!flag) {
+            	
+            	
 
             // Paso 3: Busca y actualiza los elementos hijos si ya existen
          // Obtener nodos para los atributos de Videojuego
-            Node tituloNode = rootElement.getElementsByTagName("titulo").item(0);
-            Node desarrolladorNode = rootElement.getElementsByTagName("desarrollador").item(0);
-            Node directorNode = rootElement.getElementsByTagName("director").item(0);
-            Node productorNode = rootElement.getElementsByTagName("productor").item(0);
-            Node generoNode = rootElement.getElementsByTagName("genero").item(0);
-            Node subgeneroNode = generoNode.getFirstChild();
-            Node pegiNode = rootElement.getElementsByTagName("pegi").item(0);
-            Node annoNode = rootElement.getElementsByTagName("anno").item(0);
-            Node plataformaNode = rootElement.getElementsByTagName("plataforma").item(0);
-            Node jugadoresNode = rootElement.getElementsByTagName("jugadores").item(0);
-           
+            Node tituloNode = rootElement.getElementsByTagName("titulo").item(nodo);
             
+            if ((tituloNode.toString().equalsIgnoreCase(videojuegoAEscribir.getTitulo()))||(tituloNode==null)) {
+            	
+            	escribirDatos(videojuegoAEscribir, factory, builder, doc, rootElement, tituloNode);
+            	flag=true;
             
-            for (int i = 0; i < videojuegoAEscribir.getJugadores().length; i++) {
-            
-             jugadoresNode.getChildNodes().item(i).setTextContent(Integer.toString(videojuegoAEscribir.getJugadores()[i]));
-             
-            }
-
-
-            if (tituloNode != null) {
-                tituloNode.setTextContent(videojuegoAEscribir.getTitulo());
-            }
-
-            if (desarrolladorNode != null) {
-                desarrolladorNode.setTextContent(videojuegoAEscribir.getDesarrollador());
-            }
-
-            if (directorNode != null) {
-                directorNode.setTextContent(videojuegoAEscribir.getDirector());
-            }
-
-            if (productorNode != null) {
-                productorNode.setTextContent(videojuegoAEscribir.getProductor());
-            }
-
-            if (pegiNode != null) {
-                pegiNode.setTextContent(Integer.toString(videojuegoAEscribir.getPegi())); // Nuevo valor del PEGI
-            }
-
-            if (annoNode != null) {
-                annoNode.setTextContent(Integer.toString(videojuegoAEscribir.getAnno())); // Nuevo año
-            }
-
-            if (plataformaNode != null) {
-                plataformaNode.setTextContent(videojuegoAEscribir.getPlataforma()); // Nuevas plataformas
-            }
-
-            if (generoNode != null) {
-                generoNode.setTextContent(videojuegoAEscribir.getGenero().toString()); // Nuevo genero
+            }else
+            nodo++;
             }
             
-            if (subgeneroNode != null) {
-                subgeneroNode.setTextContent(videojuegoAEscribir.getSubgenero().toString()); // Nuevo subgenero
-            }
-
-
-            // Paso 4: Guarda el documento XML modificado en un archivo
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("coleccionvideojuegos.xml"));
-            transformer.transform(source, result);
-
-            System.out.println("Datos actualizados en el archivo XML.");
-
-     
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    
+    public void escribirDatos(Videojuego videojuegoAEscribir,DocumentBuilderFactory factory, DocumentBuilder builder, Document doc, Element rootElement, Node tituloNode){
+    	
+    	try {
+    	
+    	tituloNode = rootElement.getElementsByTagName("titulo").item(nodo);
+        Node desarrolladorNode = rootElement.getElementsByTagName("desarrollador").item(nodo);
+        Node directorNode = rootElement.getElementsByTagName("director").item(nodo);
+        Node productorNode = rootElement.getElementsByTagName("productor").item(nodo);
+        Node generoNode = rootElement.getElementsByTagName("genero").item(nodo);
+        Node subgeneroNode = generoNode.getFirstChild();
+        Node pegiNode = rootElement.getElementsByTagName("pegi").item(nodo);
+        Node annoNode = rootElement.getElementsByTagName("anno").item(nodo);
+        Node plataformaNode = rootElement.getElementsByTagName("plataforma").item(nodo);
+        Node jugadoresNode = rootElement.getElementsByTagName("jugadores").item(nodo);
+       
+        
+        
+        for (int i = 0; i < videojuegoAEscribir.getJugadores().length; i++) {
+        
+         jugadoresNode.getChildNodes().item(i).setTextContent(Integer.toString(videojuegoAEscribir.getJugadores()[i])); //jugadores de cada mes
+         
+        }
+        
+        tituloNode.setTextContent(videojuegoAEscribir.getTitulo()); //nuevo titulo
+        
+        desarrolladorNode.setTextContent(videojuegoAEscribir.getDesarrollador()); //nuevo desarrollador
+        
+        directorNode.setTextContent(videojuegoAEscribir.getDirector()); //nuevo director
+        
+        productorNode.setTextContent(videojuegoAEscribir.getProductor()); //nuevo productor
+        
+        pegiNode.setTextContent(Integer.toString(videojuegoAEscribir.getPegi())); // Nuevo valor del PEGI
+        
+        annoNode.setTextContent(Integer.toString(videojuegoAEscribir.getAnno())); // Nuevo año
+        
+        plataformaNode.setTextContent(videojuegoAEscribir.getPlataforma()); // Nuevas plataformas
+        
+        generoNode.setTextContent(videojuegoAEscribir.getGenero().toString()); // Nuevo genero
+        
+        subgeneroNode.setTextContent(videojuegoAEscribir.getSubgenero().toString()); // Nuevo subgenero
+        
+
+
+        // Paso 4: Guarda el documento XML modificado en un archivo
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File("coleccionvideojuegos.xml"));
+        transformer.transform(source, result);
+
+        System.out.println("Datos actualizados en el archivo XML.");
+        
+        
+    	} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
 }
